3c789ad38dff18560f8d7170990b4682a08448ce
Arif RH <65472717+arhadi@users.noreply.github.com>
2026-06-29 20:53:34 +0700

    Update outputs.tf

--- terraform/modules/compute/outputs.tf
+++ terraform/modules/compute/outputs.tf
@@ -1,1 +1,14 @@
+output "name" {
+  value = google_compute_instance.this.name
+}
 
+output "internal_ip" {
+  value = google_compute_instance.this.network_interface[0].network_ip
+}
+
+output "external_ip" {
+  value = try(
+    google_compute_instance.this.network_interface[0].access_config[0].nat_ip,
+    ""
+  )
+}
