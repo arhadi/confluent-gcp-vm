01f315c11481923565f7596b43236a364ae08186
Arif RH <65472717+arhadi@users.noreply.github.com>
2026-06-29 20:51:19 +0700

    Add locals for common labels and tags
    Define common labels and tags for resources.

--- terraform/locals.tf
+++ terraform/locals.tf
@@ -1,1 +1,12 @@
+locals {
+  common_labels = {
+    owner       = "platform"
+    environment = "lab"
+    managed_by  = "terraform"
+    project     = "confluent"
+  }
 
+  common_tags = [
+    "ssh"
+  ]
+}
