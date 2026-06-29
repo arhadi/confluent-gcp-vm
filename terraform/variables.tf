f62ff16498bef597932a1699b54881d9eece8981
Arif RH <65472717+arhadi@users.noreply.github.com>
2026-06-29 20:49:44 +0700

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,1 +1,53 @@
+variable "project_id" {
+  type = string
+}
+
+variable "region" {
+  type = string
+}
+
+variable "zone" {
+  type = string
+}
+
+variable "vpc_name" {
+  type = string
+}
+
+variable "subnet_name" {
+  type = string
+}
+
+variable "subnet_cidr" {
+  type = string
+}
+
+variable "vms" {
+  type = map(object({
+    ip   = string
+    role = string
+    tags = list(string)
+  }))
+}
+
+variable "machine_type" {
+  type = string
+}
+
+variable "image" {
+  type = string
+}
+
+variable "disk_size" {
+  type = number
+}
+
+variable "ssh_user" {
+  type = string
+}
+
+variable "public_key_file" {
+  description = "SSH public key file"
+  type        = string
+}
 
