356e146af1515c57119f9ab326d1b5fdb4f992d0
Arif RH <65472717+arhadi@users.noreply.github.com>
2026-06-29 20:59:11 +0700

    Update variables.tf

--- terraform/modules/firewall/variables.tf
+++ terraform/modules/firewall/variables.tf
@@ -1,1 +1,36 @@
+variable "network_name" {
+  type = string
+}
 
+variable "firewall_name" {
+  type = string
+}
+
+variable "source_ranges" {
+  type = list(string)
+}
+
+variable "target_tags" {
+  type = list(string)
+  default = []
+}
+
+variable "tcp_ports" {
+  type = list(string)
+  default = []
+}
+
+variable "udp_ports" {
+  type = list(string)
+  default = []
+}
+
+variable "allow_icmp" {
+  type    = bool
+  default = true
+}
+
+variable "description" {
+  type    = string
+  default = ""
+}
