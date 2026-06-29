708f2e7ba43b8c16773ec209097131f4f2c4c8d0
Arif RH <65472717+arhadi@users.noreply.github.com>
2026-06-29 20:55:12 +0700

    Update main.tf

--- terraform/modules/network/main.tf
+++ terraform/modules/network/main.tf
@@ -1,1 +1,11 @@
+resource "google_compute_network" "this" {
+  name                    = var.vpc_name
+  auto_create_subnetworks = false
+}
 
+resource "google_compute_subnetwork" "this" {
+  name          = var.subnet_name
+  region        = var.region
+  ip_cidr_range = var.subnet_cidr
+  network       = google_compute_network.this.id
+}
