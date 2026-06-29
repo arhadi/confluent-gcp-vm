32a5a905d1bb3ba9b9c270e118d39afba07cf08c
Arif RH <65472717+arhadi@users.noreply.github.com>
2026-06-29 20:54:18 +0700

    Add variables for compute module in Terraform

--- terraform/modules/compute/variables.tf
+++ terraform/modules/compute/variables.tf
@@ -1,1 +1,54 @@
+variable "vm_name" {
+  type = string
+}
 
+variable "zone" {
+  type = string
+}
+
+variable "machine_type" {
+  type = string
+}
+
+variable "image" {
+  type = string
+}
+
+variable "disk_size" {
+  type = number
+}
+
+variable "network" {
+  type = string
+}
+
+variable "subnetwork" {
+  type = string
+}
+
+variable "internal_ip" {
+  type = string
+}
+
+variable "assign_public_ip" {
+  type    = bool
+  default = true
+}
+
+variable "ssh_user" {
+  type = string
+}
+
+variable "public_key_file" {
+  type = string
+}
+
+variable "tags" {
+  type    = list(string)
+  default = []
+}
+
+variable "labels" {
+  type    = map(string)
+  default = {}
+}
